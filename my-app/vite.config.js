69f97dd6957d3c42afceb95a7f7fe5431f692b72
Kajalkansal30 <kansalkajal.30@gmail.com>
2025-05-06 19:15:14 +0530

    fix

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -33,16 +33,45 @@
 //   },
 // });
 
+// import path from 'path';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       // No externalization needed for axios here
+//     },
+//   },
+//   css: {
+//     postcss: './postcss.config.cjs',
+//   },
+// });
+
+
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import InjectCSS from '@itsy/vite-css-inject';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    InjectCSS(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@tests': path.resolve(__dirname, './tests'),  // Added alias for tests
     },
   },
   build: {
